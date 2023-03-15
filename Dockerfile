41709720246730c042a0c36f89e28d71ffaaff05
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-03-15 18:50:16 +0100

    Add check for mandatory arguments and options.

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL maintainer='Matthias Zepper, National Genomics Infrastructure Stockholm'
 WORKDIR /bin
 RUN apk add --no-cache bash file gawk pigz
 
-COPY ./umi-injector.sh /bin/umi-injector.sh
+COPY ./src/umi-injector.sh /bin/umi-injector.sh
 RUN chmod +x /bin/umi-injector.sh
 ENTRYPOINT ["/bin/umi-injector.sh"]
 
--- src/umi-injector.sh
+++ src/umi-injector.sh
@@ -16,11 +16,12 @@ version="0.0.1"
 
 ###### parse the options and their arguments #######################################################
 
-arg_invalid() { echo -e "\x01\033[0;31m\x02 $* \x01\033[0m\x02" >&2; exit 2; }  # print complain to stderr and exit with error
-arg_required() { if [ -z "$OPTARG" ]; then arg_invalid "No argument was provided for the --$OPT option"; fi; }
-arg_requirevalid() { if [ ! -e "$OPTARG" ]; then arg_invalid "Argument to --$OPT option is not a valid file"; fi; }
-arg_overwrite() { if [ -e "$OPTARG" ]; then arg_invalid " $OPTARG already exists! Please choose --$OPT differently"; fi; }
-arg_dispensable() { if [ -n "$OPTARG" ]; then arg_invalid "No argument is allowed for the --$OPT option"; fi; }
+arg_invalid() { echo -e "\x01\033[1;31m\x02 Error: $* \x01\033[0m\x02" >&2; exit 2; }  # print complain to stderr and exit with error
+arg_required() { if [ -z "$OPTARG" ]; then arg_invalid "No argument was provided for the -$OPT option"; fi; }
+arg_requirevalid() { if [[ ! -e "$OPTARG" ]] && [[ ! -L "$OPTARG" ]]; then arg_invalid "Argument to -$OPT option is not a valid file"; fi; }
+arg_overwrite() { if [ -e "$OPTARG" ]; then arg_invalid " $OPTARG already exists! Please choose -$OPT differently"; fi; }
+arg_dispensable() { if [ -n "$OPTARG" ]; then arg_invalid "No argument is allowed for the -$OPT option"; fi; }
+paired() { if [[ -n "$in2" ]] || [[ -n "$out2" ]]; then paired=true; echo "in2 out2"; fi; } # If either pair option in set, they must have arguments as well. 
 set_defaults() { verbose="${verbose:-false}"; threads="${threads:-1}"; sep="${sep:-:}"; }
 
 # print help when -h or --help is invoked
@@ -32,13 +33,13 @@ help() {
     printf "${strfmt}" \
                 "-h / --help"  "prints this help."  "" \
                 "-v / --verbose"  "reports some statistics to stdout"   "${verbose}" \
-                "-1 / --in1"  "path to a FastQ file with reads"   "${in1}" \
-                "-2 / --in2"  "optional FastQ file with mates"   "${in2}" \
-                "-3 / --out1" "path to write the output to"   "${out1}" \
-                "-4 / --out2" "optional output for the mated reads"   "${out2}" \
-                "-u / --umi" "FastQ file with the matched UMIs"   "${umi}" \
-                "-n / --threads" "threads for parallel (de)compression"   "${threads}" \
-                "-s / --sep" "separator between readname and UMI"   "${sep}" 
+                "-1 / --in1="  "path to a FastQ file with reads"   "${in1}" \
+                "-2 / --in2="  "optional FastQ file with mates"   "${in2}" \
+                "-3 / --out1=" "path to write the output to"   "${out1}" \
+                "-4 / --out2=" "optional output for the mated reads"   "${out2}" \
+                "-u / --umi=" "FastQ file with the matched UMIs"   "${umi}" \
+                "-n / --threads=" "threads for parallel (de)compression"   "${threads}" \
+                "-s / --sep=" "separator between readname and UMI"   "${sep}" 
     echo -e "\n  Lycka till, ${USER}!"
     exit 0; }
 
@@ -69,6 +70,22 @@ do
 done
 shift $((OPTIND-1)) # remove what was already parsed from $@ list
 
-set_defaults # set default values
+# set default values for all arguments of optional options.
+set_defaults 
+
+# check that the required arguments to in1, out1 and umi have been provided
+# if either --in2 or --out2 has been provided, set paired mode and include in check.
+
+for arg in in1 out1 umi $(paired) ; do
+    if [ -z "${!arg:-}" ] ; then
+        missing+=", --$arg"
+    fi
+done
+if [ -n "${missing}" ] ; then
+    arg_invalid "${missing##,} must be provided!" #trim the first comma from missing
+fi;
+
+
+###### process the files with awk and paste ########################################################
 
 echo "Work in progress!"
